350ba6c381626e701e27cc8b31a0b33f01737be0
Stephanie Sison <sjsison27@icloud.com>
2023-04-27 20:42:45 -0700

    commit tree 4ebfb906b27c559bb529115dc333911be68c3baf

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # created all RESTful routes needed and mapped them to appropriate controller action
+  resources :birds, only: [:index, :show]
+
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
 end
